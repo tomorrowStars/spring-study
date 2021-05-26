a42b0311eda519dcb4fcf9f5526438afaaa48359
tomorrowStars <chenhongsheng2012@163.com>
2021-05-27 00:11:22 +0800

    spring-06-annotation-02:基于Java类进行 注解配置

--- spring-06-annotation-02/src/main/java/com/kuang/config/MyConfig.java
+++ spring-06-annotation-02/src/main/java/com/kuang/config/MyConfig.java
@@ -1,14 +1,28 @@
 package com.kuang.config;
 
 import com.kuang.pojo.Dog;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
 public class MyConfig {
 
+    /**
+     * 创建第三方bean
+     */
     @Bean
     public Dog dog() {
         return new Dog();
     }
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Dog dog = context.getBean(Dog.class);
+        dog.shout();
+    }
+
 }
